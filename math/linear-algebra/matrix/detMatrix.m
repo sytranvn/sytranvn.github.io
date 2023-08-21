cfa24c1497b69a3896a666f592bdbf49043086f2
sytranvn <sytrancsvn@gmail.com>
2023-08-21 13:54:00 +0700

    23-08-21 determ

---
+++ math/linear-algebra/matrix/detMatrix.m
@@ -0,0 +1,14 @@
+function [ d ] = detMatrix(m)
+	r = rows(m);
+	c = rows(m);
+	assert(r == c, "Not a square matrix");
+	d = zeros(r, r);
+	for _r = 1: r
+		for _c = 1: c
+			_m = m;
+			_m(_r,:) = [];
+			_m(:,_c) = [];
+			d(_r, _c) = det(_m);
+		end
+	end
+end
